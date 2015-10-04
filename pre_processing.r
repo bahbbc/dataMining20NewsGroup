eb06e5299d6e716116966cf58e640fdec764a6a9
Barbara Barbosa <barbarab.claudino@gmail.com>
2015-10-04 16:09:05 -0300

    Add initial test for stop_words

--- pre_processing.r
+++ pre_processing.r
@@ -11,7 +11,7 @@ tokenizing <- function(original_files, concatenated_files){
     text <-read.table(file, sep="\n", quote="", header=FALSE, stringsAsFactors=FALSE, fileEncoding="latin1")
 
     #change the list to a vector with all the chars
-    formatted_text = paste(unlist(text), collapse='')
+    formatted_text = paste(unlist(text), collapse=)
     #Tokenizing
     #lower case all strings AND replace all digits with 'D'
     formatted_text = gsub("[0-9]", " ", tolower(formatted_text))
@@ -25,12 +25,29 @@ tokenizing <- function(original_files, concatenated_files){
   concatenated_files
 }
 
+#StopWords
+english_stop_words = c('i','my','myself','we','us','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she',
+  'her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am',
+  'is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','would','could','should','ought','might','however',
+  'will','would','shall','should','can','could','may','might','must','ought','im','youre','hes','shes', 'its','were', 'theyre','ive', 'youve','weve',
+  'theyve', 'id','youd','hed','shed','wed','theyd','ill','youll', 'hell','shell','well','theyll','isnt','arent','wasnt','werent','hasnt', 'havent',
+  'hadnt','doesnt','dont','didnt','wont','wouldnt','shant','shouldnt','cant','cannot','couldnt','mustnt','lets','thats','whos','whats','heres',
+  'theres','whens','wheres','whys','hows','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with',
+  'about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over',
+  'under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some',
+  'such','no','nor','not','only','own','same','so','than','too','very','one','every','least','less','many','now','ever','never','say','says','said',
+  'also','get','go','goes','just','made','make','put','see','seen','whether','like','well','back','even','still','way','take','since','another',
+  'however','two','three','four','five','first','second','new','old','high','long','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+  'p','q','r','s','t','u','v','x','w','y','z','re')
+
+
+
 #Stemmer
 library(SnowballC)
 
 stemming <- function(concatenated_files) {}
   for (file in tokenized_text){
-    #separe the original vector in a char vector, than unlist to be able to use the stem
+    #separe the original vector in a char vector, then unlist to be able to use the stem
     vectorized_text = wordStem(unlist(strsplit(file, " ")))
 
     concatenated_files <- append(concatenated_files, paste(vectorized_text, collapse=" "))
@@ -50,7 +67,6 @@ tokenized_text <- tokenizing(file_names, c())
 
 stemmed_files <- stemming(c())
 
-#StopWords
 
 
 
